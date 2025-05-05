2a03909d92f4d1e77a15fe1bca4ff3d1a7b0476f
Neiflheim <isambart.hugo0912@gmail.com>
2025-05-05 16:21:02 +0200

    MinorFix

--- Assets/Hugo.I/Scripts/Lobby/PlayerInputManagerHandler.cs
+++ Assets/Hugo.I/Scripts/Lobby/PlayerInputManagerHandler.cs
@@ -27,23 +27,7 @@ namespace Hugo.I.Scripts.Lobby
         private void OnPlayerJoined(PlayerInput playerInput)
         {
             GameManager.Players.Add(playerInput.gameObject);
-            
-            if (playerInput.playerIndex == 0)
-            {
-                playerInput.gameObject.GetComponent<PlayerController>().PlayerId = 0;
-            }
-            if (playerInput.playerIndex == 1)
-            {
-                playerInput.gameObject.GetComponent<PlayerController>().PlayerId = 1;
-            }
-            if (playerInput.playerIndex == 2)
-            {
-                playerInput.gameObject.GetComponent<PlayerController>().PlayerId = 2;
-            }
-            if (playerInput.playerIndex == 3)
-            {
-                playerInput.gameObject.GetComponent<PlayerController>().PlayerId = 3;
-            }
+            playerInput.gameObject.GetComponent<PlayerController>().PlayerId = playerInput.playerIndex;
         }
     }
 }
