42622888dd9b5c804c3fced1ccaf30eeebba0e6a
Neiflheim <isambart.hugo0912@gmail.com>
2025-05-21 23:46:05 +0200

    MinorFix

--- Assets/Hugo.I/Scripts/Player/PlayerController.cs
+++ Assets/Hugo.I/Scripts/Player/PlayerController.cs
@@ -504,9 +504,9 @@ namespace Hugo.I.Scripts.Player
             _canvasHandler.OnSceneLoaded();
             _playerWorldSpaceDisplayInteractions.HideInteractionsButton();
             
-            _inventory[ResourcesEnum.Stone] = 200;
-            _inventory[ResourcesEnum.Metal] = 200;
-            _inventory[ResourcesEnum.ElectricalCircuit] = 200;
+            _inventory[ResourcesEnum.Stone] = 0;
+            _inventory[ResourcesEnum.Metal] = 0;
+            _inventory[ResourcesEnum.ElectricalCircuit] = 0;
         }
 
         private IEnumerator TmeBeforeCollecting(string interactableName)
