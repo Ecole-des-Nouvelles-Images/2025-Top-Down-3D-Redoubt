0868388be96e1cd1b8209aba1a36f3dda4b358d5
Neiflheim <isambart.hugo0912@gmail.com>
2025-05-27 15:51:01 +0200

    Started_Lobby

---
+++ Assets/Hugo.I/Scripts/Lobby/LaunchGameHandler.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Hugo.I.Scripts.Lobby
+{
+    public class LaunchGameHandler : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private float _timeToWait;
+        [SerializeField] private List<Collider> _players = new List<Collider>();
+        
+        private Collider _collider;
+        
+        private void Awake()
+        {
+            _collider = GetComponent<Collider>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _players.Add(other);
+
+                if (_players.Count >= 4)
+                {
+                    StartCoroutine(nameof(LaunchGame));
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _players.Remove(other);
+                StopCoroutine(nameof(LaunchGame));
+            }
+        }
+
+        private IEnumerable LaunchGame()
+        {
+            yield return new WaitForSeconds(_timeToWait);
+            SceneManager.LoadScene(2);
+        }
+    }
+}
