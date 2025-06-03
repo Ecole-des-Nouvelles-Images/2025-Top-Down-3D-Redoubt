f8ea7dd567f20fca5add0697514b101d9df33b8f
Neiflheim <isambart.hugo0912@gmail.com>
2025-06-03 16:49:18 +0200

    Added_SoundManager

---
+++ Assets/Hugo.I/Scripts/Sounds/SoundManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Hugo.I.Scripts.Utils;
+using UnityEngine;
+
+namespace Hugo.I.Scripts.Sounds
+{
+    public class SoundManager : MonoBehaviourSingleton<SoundManager>
+    {
+        [Header("<size=14><color=#E74C3C>   SOUNDS</color></size>")]
+        [Space(3)]
+        [Header("<size=13><color=#58D68D>Ambiance</color></size>")]
+        [SerializeField] public List<AudioClip> AmbianceSounds;
+        
+        [Header("<size=13><color=#58D68D>Player</color></size>")]
+        [SerializeField] public List<AudioClip> PlayerFootStepSounds;
+        [SerializeField] public List<AudioClip> PlayerCollectSounds;
+        
+        [Header("<size=13><color=#58D68D>Enemy</color></size>")]
+        [SerializeField] public List<AudioClip> EnemyFootStepSounds;
+        
+        [Header("<size=13><color=#58D68D>Tower</color></size>")]
+        [SerializeField] public List<AudioClip> LevelUpSounds;
+        [SerializeField] public List<AudioClip> HealingSounds;
+        [SerializeField] public List<AudioClip> ReloadingSounds;
+        
+        [Header("<size=13><color=#58D68D>Resource</color></size>")]
+        [SerializeField] public List<AudioClip> BreakingSounds;
+        
+        [Header("<size=13><color=#58D68D>Bullet</color></size>")]
+        [SerializeField] public List<AudioClip> ImpactSounds;
+        
+        public void PlaySound(GameObject obj, List<AudioClip> clips)
+        {
+            AudioSource audio = obj.AddComponent<AudioSource>();
+            audio.clip = clips[Random.Range(0, clips.Count)];
+            audio.Play();
+            float durationClip = audio.clip.length;
+            StartCoroutine(RemoveAudioSource(durationClip, audio));
+        }
+        
+        public IEnumerator RemoveAudioSource(float timer, AudioSource source)
+        {
+            yield return new WaitForSeconds(timer);
+            Destroy(source);
+        }
+    }
+}
